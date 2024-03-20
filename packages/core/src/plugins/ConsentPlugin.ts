segmentio/analytics-react-native

fix: remove circular dependency in ConsentPlugin

packages/core/src/plugins/ConsentPlugin.ts
@@ -1,13 +1,13 @@
-import {
-  Plugin,
-  type SegmentClient,
-  type DestinationPlugin,
+import { Plugin } from '../plugin';
+import { PluginType } from '../types';
+import type {
   IntegrationSettings,
-  PluginType,
   SegmentAPIIntegration,
   SegmentEvent,
   TrackEventType,
-} from '..';
+} from '../types';
+import type { DestinationPlugin } from '../plugin';
+import type { SegmentClient } from '../analytics';
 
 import { SEGMENT_DESTINATION_KEY } from './SegmentDestination';
 

ht-sdks/events-sdk-react-native

packages/core/src/plugins/ConsentPlugin.ts
@@ -1,13 +1,13 @@
-import {
-  Plugin,
-  type HightouchClient,
-  type DestinationPlugin,
+import { Plugin } from '../plugin';
+import { PluginType } from '../types';
+import type {
   IntegrationSettings,
-  PluginType,
   HightouchAPIIntegration,
   HightouchEvent,
   TrackEventType,
-} from '..';
+} from '../types';
+import type { DestinationPlugin } from '../plugin';
+import type { HightouchClient } from '../analytics';
 
 import { HIGHTOUCH_DESTINATION_KEY } from './HightouchDestination';
 