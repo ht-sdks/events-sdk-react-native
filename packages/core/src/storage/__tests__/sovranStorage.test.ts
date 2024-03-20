segmentio/analytics-react-native

fix: preserve pending events when initialization doesn't complete (offline) (#916)
* fix: preserve pending events when initialization doesn't complete (offline)
* chore: auto fix linter issues

packages/core/src/storage/__tests__/sovranStorage.test.ts
@@ -4,15 +4,20 @@ import { createCallbackManager as mockCreateCallbackManager } from '../../test-h
 import { SovranStorage } from '../sovranStorage';
 
 import type { Persistor } from '@segment/sovran-react-native';
-import type { Context, DeepPartial } from '../../types';
+import {
+  EventType,
+  type Context,
+  type DeepPartial,
+  SegmentEvent,
+} from '../../types';
 jest.mock('@segment/sovran-react-native', () => ({
   registerBridgeStore: jest.fn(),
   createStore: <T extends object>(initialState: T) => {
     const callbackManager = mockCreateCallbackManager<T>();
 
-    let store = {
-      ...initialState,
-    };
+    let store: T = Array.isArray(initialState)
+      ? ([...initialState] as T)
+      : ({ ...initialState } as T);
 
     return {
       subscribe: jest
@@ -30,7 +35,9 @@ jest.mock('@segment/sovran-react-native', () => ({
             return store;
           }
         ),
-      getState: jest.fn().mockImplementation(() => ({ ...store })),
+      getState: jest.fn().mockImplementation(() => {
+        return Array.isArray(store) ? [...store] : { ...store };
+      }),
     };
   },
 }));
@@ -124,5 +131,24 @@ describe('sovranStorage', () => {
       storePersistor: CustomPersistor,
     });
     await commonAssertions(sovran);
+  });
+
+  it('adds/removes pending events', async () => {
+    const sovran = new SovranStorage({ storeId: 'test' });
+    console.log(sovran.pendingEvents.get());
+
+    // expect(sovran.pendingEvents.get().length).toBe(0);
+
+    const event: SegmentEvent = {
+      messageId: '1',
+      type: EventType.TrackEvent,
+      event: 'Track',
+    };
+    await sovran.pendingEvents.add(event);
+
+    expect(sovran.pendingEvents.get().length).toBe(1);
+
+    await sovran.pendingEvents.remove(event);
+    expect(sovran.pendingEvents.get().length).toBe(0);
   });
 });

packages/core/src/storage/sovranStorage.ts
@@ -28,21 +28,19 @@ import type {
   Settable,
   Dictionary,
   ReadinessStore,
+  Queue,
 } from './types';
 
 type Data = {
-  events: SegmentEvent[];
-  eventsToRetry: SegmentEvent[];
   context: DeepPartial<Context>;
   settings: SegmentAPIIntegrations;
   consentSettings: SegmentAPIConsentSettings | undefined;
   userInfo: UserInfoState;
   filters: DestinationFilters;
+  pendingEvents: SegmentEvent[];
 };
 
 const INITIAL_VALUES: Data = {
-  events: [],
-  eventsToRetry: [],
   context: {},
   settings: {},
   consentSettings: undefined,
@@ -52,6 +50,7 @@ const INITIAL_VALUES: Data = {
     userId: undefined,
     traits: undefined,
   },
+  pendingEvents: [],
 };
 
 const isEverythingReady = (state: ReadinessStore) =>
@@ -115,9 +114,9 @@ const addAnonymousId =
   };
 
 function createStoreGetter<
-  U extends Record<string, unknown>,
+  U extends object,
   Z extends keyof U | undefined = undefined,
-  V = undefined,
+  V = undefined
 >(store: Store<U>, key?: Z): getStateFunc<Z extends keyof U ? V : U> {
   type X = Z extends keyof U ? V : U;
   return createGetter(
@@ -151,6 +150,7 @@ export class SovranStorage implements Storage {
   private userInfoStore: Store<{ userInfo: UserInfoState }>;
   private deepLinkStore: Store<DeepLinkData> = deepLinkStore;
   private filtersStore: Store<DestinationFilters>;
+  private pendingStore: Store<SegmentEvent[]>;
 
   readonly isReady: Watchable<boolean>;
 
@@ -172,6 +172,10 @@ export class SovranStorage implements Storage {
 
   readonly deepLinkData: Watchable<DeepLinkData>;
 
+  readonly pendingEvents: Watchable<SegmentEvent[]> &
+    Settable<SegmentEvent[]> &
+    Queue<SegmentEvent, SegmentEvent[]>;
+
   constructor(config: StorageConfig) {
     this.storeId = config.storeId;
     this.storePersistor = config.storePersistor;
@@ -181,6 +185,7 @@ export class SovranStorage implements Storage {
       hasRestoredSettings: false,
       hasRestoredUserInfo: false,
       hasRestoredFilters: false,
+      hasRestoredPendingEvents: false,
     });
 
     const markAsReadyGenerator = (key: keyof ReadinessStore) => () => {
@@ -382,6 +387,44 @@ export class SovranStorage implements Storage {
           return { userInfo: newState };
         });
         return userInfo;
+      },
+    };
+
+    // Pending Events
+    this.pendingStore = createStore<SegmentEvent[]>(
+      INITIAL_VALUES.pendingEvents,
+      {
+        persist: {
+          storeId: `${this.storeId}-pendingEvents`,
+          persistor: this.storePersistor,
+          saveDelay: this.storePersistorSaveDelay,
+          onInitialized: markAsReadyGenerator('hasRestoredPendingEvents'),
+        },
+      }
+    );
+
+    this.pendingEvents = {
+      get: createStoreGetter(this.pendingStore),
+      onChange: (callback: (value: SegmentEvent[]) => void) =>
+        this.pendingStore.subscribe((store) => callback(store)),
+      set: async (value) => {
+        return await this.pendingStore.dispatch((state) => {
+          let newState: SegmentEvent[];
+          if (value instanceof Function) {
+            newState = value(state);
+          } else {
+            newState = [...value];
+          }
+          return newState;
+        });
+      },
+      add: (event: SegmentEvent) => {
+        return this.pendingStore.dispatch((events) => [...events, event]);
+      },
+      remove: (event: SegmentEvent) => {
+        return this.pendingStore.dispatch((events) =>
+          events.filter((e) => e.messageId != event.messageId)
+        );
       },
     };
 

ht-sdks/events-sdk-react-native

Co-Authored-By: Kevin Lin <kevin@hightouch.io>

packages/core/src/storage/__tests__/sovranStorage.test.ts
@@ -4,15 +4,20 @@ import { createCallbackManager as mockCreateCallbackManager } from '../../test-h
 import { SovranStorage } from '../sovranStorage';
 
 import type { Persistor } from '@ht-sdks/sovran-react-native';
-import type { Context, DeepPartial } from '../../types';
+import {
+  EventType,
+  type Context,
+  type DeepPartial,
+  HightouchEvent,
+} from '../../types';
 jest.mock('@ht-sdks/sovran-react-native', () => ({
   registerBridgeStore: jest.fn(),
   createStore: <T extends object>(initialState: T) => {
     const callbackManager = mockCreateCallbackManager<T>();
 
-    let store = {
-      ...initialState,
-    };
+    let store: T = Array.isArray(initialState)
+      ? ([...initialState] as T)
+      : ({ ...initialState } as T);
 
     return {
       subscribe: jest
@@ -30,7 +35,9 @@ jest.mock('@ht-sdks/sovran-react-native', () => ({
             return store;
           }
         ),
-      getState: jest.fn().mockImplementation(() => ({ ...store })),
+      getState: jest.fn().mockImplementation(() => {
+        return Array.isArray(store) ? [...store] : { ...store };
+      }),
     };
   },
 }));
@@ -124,5 +131,24 @@ describe('sovranStorage', () => {
       storePersistor: CustomPersistor,
     });
     await commonAssertions(sovran);
+  });
+
+  it('adds/removes pending events', async () => {
+    const sovran = new SovranStorage({ storeId: 'test' });
+    console.log(sovran.pendingEvents.get());
+
+    // expect(sovran.pendingEvents.get().length).toBe(0);
+
+    const event: HightouchEvent = {
+      messageId: '1',
+      type: EventType.TrackEvent,
+      event: 'Track',
+    };
+    await sovran.pendingEvents.add(event);
+
+    expect(sovran.pendingEvents.get().length).toBe(1);
+
+    await sovran.pendingEvents.remove(event);
+    expect(sovran.pendingEvents.get().length).toBe(0);
   });
 });

packages/core/src/storage/sovranStorage.ts
@@ -28,21 +28,19 @@ import type {
   Settable,
   Dictionary,
   ReadinessStore,
+  Queue,
 } from './types';
 
 type Data = {
-  events: HightouchEvent[];
-  eventsToRetry: HightouchEvent[];
   context: DeepPartial<Context>;
   settings: HightouchAPIIntegrations;
   consentSettings: HightouchAPIConsentSettings | undefined;
   userInfo: UserInfoState;
   filters: DestinationFilters;
+  pendingEvents: HightouchEvent[];
 };
 
 const INITIAL_VALUES: Data = {
-  events: [],
-  eventsToRetry: [],
   context: {},
   settings: {},
   consentSettings: undefined,
@@ -52,6 +50,7 @@ const INITIAL_VALUES: Data = {
     userId: undefined,
     traits: undefined,
   },
+  pendingEvents: [],
 };
 
 const isEverythingReady = (state: ReadinessStore) =>
@@ -115,7 +114,7 @@ const addAnonymousId =
   };
 
 function createStoreGetter<
-  U extends Record<string, unknown>,
+  U extends object,
   Z extends keyof U | undefined = undefined,
   V = undefined,
 >(store: Store<U>, key?: Z): getStateFunc<Z extends keyof U ? V : U> {
@@ -151,6 +150,7 @@ export class SovranStorage implements Storage {
   private userInfoStore: Store<{ userInfo: UserInfoState }>;
   private deepLinkStore: Store<DeepLinkData> = deepLinkStore;
   private filtersStore: Store<DestinationFilters>;
+  private pendingStore: Store<HightouchEvent[]>;
 
   readonly isReady: Watchable<boolean>;
 
@@ -172,6 +172,10 @@ export class SovranStorage implements Storage {
 
   readonly deepLinkData: Watchable<DeepLinkData>;
 
+  readonly pendingEvents: Watchable<HightouchEvent[]> &
+    Settable<HightouchEvent[]> &
+    Queue<HightouchEvent, HightouchEvent[]>;
+
   constructor(config: StorageConfig) {
     this.storeId = config.storeId;
     this.storePersistor = config.storePersistor;
@@ -181,6 +185,7 @@ export class SovranStorage implements Storage {
       hasRestoredSettings: false,
       hasRestoredUserInfo: false,
       hasRestoredFilters: false,
+      hasRestoredPendingEvents: false,
     });
 
     const markAsReadyGenerator = (key: keyof ReadinessStore) => () => {
@@ -382,6 +387,44 @@ export class SovranStorage implements Storage {
           return { userInfo: newState };
         });
         return userInfo;
+      },
+    };
+
+    // Pending Events
+    this.pendingStore = createStore<HightouchEvent[]>(
+      INITIAL_VALUES.pendingEvents,
+      {
+        persist: {
+          storeId: `${this.storeId}-pendingEvents`,
+          persistor: this.storePersistor,
+          saveDelay: this.storePersistorSaveDelay,
+          onInitialized: markAsReadyGenerator('hasRestoredPendingEvents'),
+        },
+      }
+    );
+
+    this.pendingEvents = {
+      get: createStoreGetter(this.pendingStore),
+      onChange: (callback: (value: HightouchEvent[]) => void) =>
+        this.pendingStore.subscribe((store) => callback(store)),
+      set: async (value) => {
+        return await this.pendingStore.dispatch((state) => {
+          let newState: HightouchEvent[];
+          if (value instanceof Function) {
+            newState = value(state);
+          } else {
+            newState = [...value];
+          }
+          return newState;
+        });
+      },
+      add: (event: HightouchEvent) => {
+        return this.pendingStore.dispatch((events) => [...events, event]);
+      },
+      remove: (event: HightouchEvent) => {
+        return this.pendingStore.dispatch((events) =>
+          events.filter((e) => e.messageId != event.messageId)
+        );
       },
     };
 